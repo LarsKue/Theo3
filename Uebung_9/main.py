0cd4a0ca17901a2009a7e78160664b9727103dd2
Lars Kue <lars@kuehmichel.de>
2020-01-05 16:57:29 +0100

    Comments

--- Uebung_9/main.py
+++ Uebung_9/main.py
@@ -18,6 +18,7 @@ def gamma(i: int) -> float:
 
 
 def gen_L(N: int) -> np.ndarray:
+    # generates the Matrix L
     result = np.zeros((N, N))
     for i in range(N - 1):
         result[i + 1][i] = 1
@@ -28,6 +29,7 @@ def gen_L(N: int) -> np.ndarray:
 
 
 def gen_R(N: int) -> np.ndarray:
+    # generates the Matrix R (also called U in the paper)
     result = np.identity(N)
     for i in range(N - 1):
         result[i][i + 1] = gamma(i + 1)
@@ -36,11 +38,13 @@ def gen_R(N: int) -> np.ndarray:
 
 
 def delta_phi(x):
+    # this is the right hand side of the poisson-equation
     # return -4 * pi * rho(x)
     return x - 1
 
 
 def analyt(x, length):
+    # this is the analytical solution of the poisson-equation
     c = 1 / 2 * length - 1 / 6 * length ** 2
     return 1 / 6 * x ** 3 - 1 / 2 * x ** 2 + c * x
 
@@ -51,28 +55,33 @@ def deviation(x, y):
 
 
 def main(argv: list) -> int:
+    length = 1
+    xlit = np.linspace(0, length, 10000)
+    phi_lit = analyt(xlit, length)
+    # calculate the approximation for different N
     for N in [10, 50, 200, 500, 1000, 3000]:
-        length = 1
         h = length / N
 
         L = gen_L(N)
         R = gen_R(N)
 
         x = np.linspace(0, length, N)
-        xlit = np.linspace(0, length, 10000)
 
         dp = delta_phi(x)
 
         try:
+            # invert the L and R matrices, if possible
             LI = np.linalg.inv(L)
             RI = np.linalg.inv(R)
         except np.linalg.LinAlgError:
+            # if they aren't invertible, the poisson equation may not be solved using this algorithm
             print("Not Invertible.")
             return -1
 
+        # calculate the approximate solution for phi
         phi = h ** 2 * np.dot(np.dot(RI, LI), dp)
-        phi_lit = analyt(xlit, length)
 
+        # write solutions, analytical solutions and deviations to a file
         with open(f"{N}.txt", "w+") as f:
             phi_lit_comp = analyt(x, length)
             for i in range(N):
@@ -87,6 +96,7 @@ def main(argv: list) -> int:
         # plt.show()
         # return 0
 
+        # plot solution and analytical solution and save as image
         plt.figure(figsize=(10, 8))
         plt.plot(x, phi, label="Näherung")
         plt.plot(xlit, phi_lit, label="Analytische Lösung")
